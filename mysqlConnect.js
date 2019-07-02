eb579afad1110f3ea48990dd38ea5b255d031d45
Bassant <bassantsamy@aucegypt.edu>
2019-07-02 15:23:13 +0200

    Finished authentication and sign up

--- mysqlConnect.js
+++ mysqlConnect.js
@@ -3,8 +3,9 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'mysql',
-    database: 'Toktok-Db'
+    password: '',
+    database: 'Toktok-Db',
+    socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
 });
 
 connection.connect((error) => {
--- routes/driver.js
+++ routes/driver.js
@@ -8,34 +8,45 @@ const mysql = require('./../mysqlConnect');
 router.post('/', function(req, res, next) {
     const email = req.body.driverEmail;
     const loc = req.body.loc;
-
+    //console.log (loc);
     // Get the ride
-    mysql.query(`SELECT * FROM ride WHERE fare_s=${loc} AND state="pending";`, (error, result) => {
+    mysql.query(`SELECT * FROM ride WHERE fare_s='${loc}' AND state="pending";`, (error, result) => {
         if (error) {
             console.log(error);
-            res.status(404).end();  // TODO:: Change status code
+            res.status(404).end("Ride Not Found!");
         }
-        else if(result.length !== 0){
-            mysql.query(`UPDATE INTO ride(state, driveremail) VALUES("running", ${email}) WHERE rideid=${result[0].rideId};`, (err, updateResult=>{
-                if(err)
+        else if (result.length !== 0) {
+            mysql.query(`UPDATE  ride SET state = "running", driveremail ="${email}" WHERE rideid=${result[0].rideid};`, (err, updateResult) => {
+                if (err)
                     console.log(err);
-            }));
+            });
 
-            mysql.query(`SELECT * FROM rider WHERE email IN (SELECT rideremail FROM ride WHERE driveremail=${email})`, (err, updateResult=>{
-                if(err)
-                    console.log(err);
-                else {
-                    const resBody = {
-                        name: updateResult[0].name,
-                        endLoc: result[0].fareE
-                    };
-                    res.status(200).json(resBody);
+            mysql.query(`SELECT * FROM rider WHERE email IN (SELECT rideremail FROM ride WHERE driveremail='${email}' AND state="running")`, (error, updateResult) => {
+                if (error) {
+                    console.log(error);
+                    res.status(404).end("Rider Not Found!");
                 }
-            }));
+                else {
+                    mysql.query(`SELECT * FROM fare WHERE fstart = '${loc}' AND fend ='${result[0].fare_e}';`, (e, fareRes) => {
+                        if (e) {
+                            console.log(e);
+                            res.status(404).end("Fare Not Found!");
+                        }
+                        else {
+                            const resBody =
+                                {
+                                    name: updateResult[0].name,
+                                    endLoc: result[0].fare_e,
+                                    fare: fareRes[0].price
+                                };
+                            res.status(200).json(resBody);
+                        }
+                    });
+                }
+            });
         }
-        else {
-            res.status(404).end("Not Found!");  // TODO:: Change status code
-        }
+        else
+            res.status(404).end("Ride Not Found!");
     });
 });
 
