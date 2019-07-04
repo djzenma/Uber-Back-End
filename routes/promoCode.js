ded9307c802284fc462d3a23ce532ba656bdbbd8
djzenma <eidma@aucegypt.edu>
2019-07-04 17:11:12 +0200

    Server can now Add, Delete, Update Promo codes (handle Admin requests to modify promo codes)

---
+++ routes/promoCode.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var router = express.Router();
+const mysql = require('./../mysqlConnect');
+
+
+
+router.post('/add/', (req,res,next) => {
+    const code = req.body.code;
+    const amount = req.body.amount;
+
+    mysql.query(`INSERT INTO discountcode(dcode, value) VALUES('${code}',${amount});`, (error, result)=>{
+        if(error) {
+            console.log(error);
+            res.status(500).end();
+        }
+        else {
+            res.status(200).end();
+        }
+    });
+});
+
+router.delete('/remove/', (req,res,next) => {
+    const code = req.body.code;
+
+    mysql.query(`DELETE FROM discountcode WHERE dcode='${code}';`, (error, result)=>{
+        if(error) {
+            console.log(error);
+            res.status(500).end();
+        }
+        else {
+            res.status(200).end("Promo Code deleted Successfully");
+        }
+    });
+});
+
+router.put('/modify/', (req,res,next) => {
+    const code = req.body.code;
+    const newAmount = req.body.amount;
+
+    mysql.query(`UPDATE discountcode SET value=${newAmount} WHERE dcode='${code}';`, (error, result)=>{
+        if(error) {
+            console.log(error);
+            res.status(500).end();
+        }
+        else {
+            res.status(200).end("Promo Code Updated Successfully");
+        }
+    });
+});
+
+
+module.exports = router;
