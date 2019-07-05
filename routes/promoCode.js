440661febacd416da34de5d70959c8699b768967
djzenma <eidma@aucegypt.edu>
2019-07-05 21:07:04 +0200

    Now the server keeps a record of relations

--- routes/promoCode.js
+++ routes/promoCode.js
@@ -9,6 +9,7 @@ const mysql = require('./../mysqlConnect');
 router.post('/add/', (req,res,next) => {
     const code = req.body.code;
     const amount = req.body.amount;
+    const admin = req.body.admin;
 
     mysql.query(`INSERT INTO discountcode(dcode, value) VALUES('${code}',${amount});`, (error, result)=>{
         if(error) {
@@ -17,6 +18,12 @@ router.post('/add/', (req,res,next) => {
         }
         else {
             res.status(200).end();
+
+            // Keep record
+            mysql.query(`INSERT INTO modifydiscountcode(changeremail, disccode, ins_upd_del) VALUES('${admin}','${code}', 0);`, (e, r) => {
+                if (e)
+                    console.log(e);
+            });
         }
     });
 });
@@ -26,14 +33,29 @@ router.post('/add/', (req,res,next) => {
  */
 router.delete('/remove/', (req,res,next) => {
     const code = req.body.code;
+    const admin = req.body.admin;
 
-    mysql.query(`DELETE FROM discountcode WHERE dcode='${code}';`, (error, result)=>{
-        if(error) {
-            console.log(error);
-            res.status(500).end();
-        }
+    mysql.query('SET FOREIGN_KEY_CHECKS=0;',(er, re)=> {
+        if (er)
+            console.log(er);
         else {
-            res.status(200).end("Promo Code deleted Successfully");
+            mysql.query(`DELETE FROM discountcode WHERE dcode='${code}';`, (error, result) => {
+                if (error) {
+                    console.log(error);
+                    res.status(500).end();
+                } else {
+                    res.status(200).end("Promo Code deleted Successfully");
+
+                    // Keep record
+                    mysql.query(`INSERT INTO modifydiscountcode(changeremail, disccode, ins_upd_del) VALUES('${admin}','${code}', 2);`, (e, r) => {
+                        if (e)
+                            console.log(e);
+                        else {
+                            mysql.query('SET FOREIGN_KEY_CHECKS=1;',(e, r)=> {});
+                        }
+                    });
+                }
+            });
         }
     });
 });
@@ -44,6 +66,7 @@ router.delete('/remove/', (req,res,next) => {
 router.put('/modify/', (req,res,next) => {
     const code = req.body.code;
     const newAmount = req.body.amount;
+    const admin = req.body.admin;
 
     mysql.query(`UPDATE discountcode SET value=${newAmount} WHERE dcode='${code}';`, (error, result)=>{
         if(error) {
@@ -52,6 +75,12 @@ router.put('/modify/', (req,res,next) => {
         }
         else {
             res.status(200).end("Promo Code Updated Successfully");
+
+            // Keep record
+            mysql.query(`INSERT INTO modifydiscountcode(changeremail, disccode, ins_upd_del) VALUES('${admin}','${code}', 1);`, (e, r) => {
+                if (e)
+                    console.log(e);
+            });
         }
     });
 });
